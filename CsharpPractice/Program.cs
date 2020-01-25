3c3375a2d5b723726329a9e82edc6057e253f734
harley garcia <hagarcia99@outlook.com>
2020-01-25 10:53:16 -0600

    Calculator class

--- CsharpPractice/Program.cs
+++ CsharpPractice/Program.cs
@@ -3,7 +3,11 @@
 namespace CsharpPractice {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+
         }
     }
 }
